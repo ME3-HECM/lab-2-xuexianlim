0052abc751ea86d2d017611a96bf1f61f668ebb1
xuexianlim <xxl19@ic.ac.uk>
2021-10-20 18:19:27 +0100

    Exercise 2.1

--- main.c
+++ main.c
@@ -4,25 +4,16 @@
 
 #include <xc.h>
 #include "LEDarray.h"
+#include "ADC.h"
 
 #define _XTAL_FREQ 64000000 //note intrinsic _delay function is 62.5ns at 64,000,000Hz  
 
 void main(void) 
 {
-	unsigned int count=0;
     LEDarray_init();
-    button_init();
-    short int release=1;
+    ADC_init();
     
     while (1) {
-		if (!PORTFbits.RF2 && release) {
-            count++;  //increment count every time button pressed and was released from previous press
-            release = 0;
-        } else if (PORTFbits.RF2) {
-            release = 1;
-        }
-        
-        if (count > 511) {count = 0;} //reset if number gets too big
-        LEDarray_disp_bin(count); //output a number on the LED array in binary
+        LEDarray_disp_bin(ADC_getval()); //get analogue value and display it on LED array in binary
     }
 }
